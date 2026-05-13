f53963234387f4db74e2edbcc12426bd8fb584bc
Zenith <zenithjdozucwuxgwn@gmail.com>
2026-05-13 10:14:18 -0400

    Create check.js

---
+++ check.js
@@ -0,0 +1,8 @@
+	// Issue a warning if trying to preview an exported project on disk.
+	(function(){
+		// Check for running exported on file protocol
+		if (window.location.protocol.substr(0, 4) === "file")
+		{
+			alert("Javon jorks it to pomni.);
+		}
+	})();
